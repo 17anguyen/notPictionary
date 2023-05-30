ffd7b0f3ddf6e1dbb29c8c2ce0c043fc40d2483e
Alivia <43556891+17anguyen@users.noreply.github.com>
2023-05-30 12:52:30 -0700

    fixed all react errors display working

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,24 +1,9 @@
 import React from 'react';
-import TextTransition, { presets } from 'react-text-transition';
-import work from '../../Assets/images/workin.gif';
-import "../css/home.css";
 
 export default function Home() {
     return (
-        <main className='homebg'>
-            <div className='row align-center'>
-
-                <div className='home-banner'>
-                    <h3>
-                        <marquee className='blink text-center' behavior="slide" direction="up">Don't Mind the Dust. This Site is Under Construction</marquee>
-                    </h3>
-                </div>
-
-                <div className='' style={{ paddingBottom: '150px' }}>
-                    <img className='mx-auto d-block' alt="progress" src={work} style={{ height: '110%' }} />
-                </div >
-
-            </div>
+        <main className=''>
+            <h1>home</h1>
         </main >
     );
 }
