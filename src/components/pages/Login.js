b0787c135aea4da78b9ef297514b75bea9adc856
Alivia <43556891+17anguyen@users.noreply.github.com>
2023-05-31 03:28:11 -0700

    finished log in page

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -6,28 +6,77 @@ import banner from '../../Assets/buttons/loginsignupbanner.svg'
 
 export default function Login() {
     return (
-        <main className='loginbg'>
-            <div className='row' style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                <div>
-                    <img src={banner}></img>
-                </div>
+        <div className='loginbg' >
+            <div className='container' style={{ width: '100%' }}>
+                {/* <h2 className='z-1 position-absolute'>Login</h2> */}
+                <img src={banner}
+                    className='mx-auto'
+                    style={{ width: '60%', height: 'auto', marginTop: '10px' }}></img>
+                <div className='centered'>
+                    <h2 className='title'>Login</h2>
 
-                <div className="card position-absolute top-50 start-50 translate-middle" style={{
-                    backgroundColor: 'black', color: 'white', border: 'solid 3px #FE00FE', borderRadius: '25px', width: '80%', height: '50%', paddingBottom: '50px', marginLeft: 'auto', marginRight: 'auto', top: '50 %', left: '50 %', transform: 'translate(-50 %, -50 %)'
-                }}>
-                    < div className="card-body" style={{ paddingTop: '220px' }}>
+                </div>
+            </div>
+
+            <div className='row'>
+
+                <div
+                    className="card position-absolute top-50 start-50 translate-middle"
+                    style={{
+                        backgroundColor: 'black', color: 'white', border: 'solid 3px #FE00FE', borderRadius: '25px', width: '80%', height: '50%',
+                    }}>
+                    < div
+                        className="card-body positon-relative"
+                        style={{ paddingTop: '10%' }}>
 
                         <form>
-                            <div className="">
-                                <label for="username" className="form-label">Username</label>
-                                <input type="input" className="form-control" id="username" style={{ borderRadius: '25px', height: '55px' }} />
+
+                            <div className="form-floating mb-3">
+                                <input type="input"
+                                    className="form-control"
+                                    id="floatingInput"
+                                    placeholder="Username"
+                                    style={{
+                                        borderRadius: '25px',
+                                        height: '55px',
+                                    }} />
+                                <label for="floatingInput"
+                                    style={{
+                                        color: '#797979'
+                                    }}>
+                                    Username
+                                </label>
                             </div>
-                            <div className="">
-                                <label for="exampleInputPassword1" className="form-label">Password</label>
-                                <input type="password" className="form-control" id="exampleInputPassword1" style={{ borderRadius: '25px', height: '55px' }} />
+
+                            <div className="form-floating">
+                                <input type="password"
+                                    className="form-control"
+                                    id="floatingPassword"
+                                    placeholder="Password"
+                                    style={{
+                                        borderRadius: '25px',
+                                        height: '55px'
+                                    }} />
+                                <label for="floatingPassword"
+                                    style={{
+                                        color: '#797979'
+                                    }}>
+                                    Password
+                                </label>
                             </div>
-                            <div className='text-center' style={{ paddingTop: '15px' }}>
-                                <button type="submit" className="btn btn-primary" style={{ borderRadius: '25px', height: '50px', backgroundColor: '#FF6E27', width: 'auto', fontSize: '25px' }}>Submit</button>
+
+                            <div
+                                className='text-center'
+                                style={{ paddingTop: '15px' }}>
+                                <button
+                                    type="submit"
+                                    className="btn btn-primary"
+                                    style={{
+                                        borderRadius: '25px', height: '50px',
+                                        backgroundColor: '#FF6E27', width: 'auto', fontSize: '25px'
+                                    }}>
+                                    Submit
+                                </button>
 
                             </div>
                         </form>
@@ -39,6 +88,7 @@ export default function Login() {
             </div>
 
 
-        </main >
+
+        </div >
     );
 }
