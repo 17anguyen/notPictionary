e428f8515bda1a62cd84584a5a4009379da818a4
Alivia <43556891+17anguyen@users.noreply.github.com>
2023-05-31 03:40:40 -0700

    merge error

--- src/components/pages/RoomSelect.js
+++ src/components/pages/RoomSelect.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import '../css/RoomSelect.css';
 
-export default function createRoom () {
+export default function createRoom() {
     return (
-        
+
         <main className='RoomSelectbg'>
             <div className="card">
-    
+
                 <div className="card-body">
                     <button type="button" class="btn btn-success btn-lg">Enter a Room Code</button>
                     <button type="button" class="btn btn-success btn-lg">Create a Room</button>
                 </div>
-                
+
             </div>
         </main>
     );
