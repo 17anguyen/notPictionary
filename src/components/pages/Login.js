42698610245b8b754281dad48a5c892535db0a33
Alivia <43556891+17anguyen@users.noreply.github.com>
2023-05-30 18:04:29 -0700

    fixed input form

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,10 +1,44 @@
 import React from 'react';
+import '../css/login.css';
+import banner from '../../Assets/buttons/loginsignupbanner.svg'
+
 
 
 export default function Login() {
     return (
-        <main className=''>
+        <main className='loginbg'>
+            <div className='row' style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                <div>
+                    <img src={banner}></img>
+                </div>
 
-        </main>
+                <div className="card position-absolute top-50 start-50 translate-middle" style={{
+                    backgroundColor: 'black', color: 'white', border: 'solid 3px #FE00FE', borderRadius: '25px', width: '80%', height: '50%', paddingBottom: '50px', marginLeft: 'auto', marginRight: 'auto', top: '50 %', left: '50 %', transform: 'translate(-50 %, -50 %)'
+                }}>
+                    < div className="card-body" style={{ paddingTop: '220px' }}>
+
+                        <form>
+                            <div className="">
+                                <label for="username" className="form-label">Username</label>
+                                <input type="input" className="form-control" id="username" style={{ borderRadius: '25px', height: '55px' }} />
+                            </div>
+                            <div className="">
+                                <label for="exampleInputPassword1" className="form-label">Password</label>
+                                <input type="password" className="form-control" id="exampleInputPassword1" style={{ borderRadius: '25px', height: '55px' }} />
+                            </div>
+                            <div className='text-center' style={{ paddingTop: '15px' }}>
+                                <button type="submit" className="btn btn-primary" style={{ borderRadius: '25px', height: '50px', backgroundColor: '#FF6E27', width: 'auto', fontSize: '25px' }}>Submit</button>
+
+                            </div>
+                        </form>
+
+
+                    </div>
+                </div >
+
+            </div>
+
+
+        </main >
     );
 }
--- src/components/pages/RoomSelect.js
+++ src/components/pages/RoomSelect.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import '../css/RoomSelect.css';
 
-export default function createRoom () {
+export default function createRoom() {
     return (
-        
+
         <main className='RoomSelectbg'>
             <div className="card">
-    
+                <h1> RoomSelect</h1>
+
                 <div className="card-body">
                     <button type="button" class="btn btn-success btn-lg">Enter a Room Code</button>
                     <button type="button" class="btn btn-success btn-lg">Create a Room</button>
                 </div>
-                
+
             </div>
         </main>
     );
