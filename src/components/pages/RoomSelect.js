bc98b14914e54823f2629139c71bc55134cdbef6
Alivia <43556891+17anguyen@users.noreply.github.com>
2023-06-02 02:07:41 -0700

    created a responsive room page

--- src/components/pages/RoomSelect.js
+++ src/components/pages/RoomSelect.js
@@ -1,20 +1,53 @@
 import React from "react";
-import "../css/RoomSelect.css";
-import top from "../../Assets/buttons/greenlargebutton.svg";
-import bottom from "../../Assets/buttons/bluesmallbutton.svg";
+import "../css/UserSelect.css";
+import top from '../../Assets/buttons/greenlargebutton.svg'
+import bottom from '../../Assets/buttons/bluesmallbutton.svg'
+import arrows from '../../Assets/page elements/arrows.svg'
+import planet from '../../Assets/page elements/planet.svg'
+
 
 export default function createRoom() {
   return (
-    <div className="RoomSelectbg">
-      <div className="container">
-        <div className="buttontop">
-          <img src={top} />
+    <div className='UserSelectbg z-n1'>
+      <img className='z-1 position-absolute top-0 start-0 mx-auto' src={planet} style={{ width: '20%' }} />
+      <img className='z-1 position-absolute bottom-0 end-0 mx-auto' src={arrows} style={{ width: '10%' }} />
+
+      <div className='select container'>
+
+        <a href='/lobby'>
+          <img className='signup'
+            src={top}
+            style={{
+              width: '100%',
+              height: '100%'
+            }} />
+        </a>
+
+        <div className='centered-signup'>
+          <h2 className='title-signup'>Create a room</h2>
+
         </div>
 
-        <div className="buttonbottom z-1" style={{ paddingTop: "20%" }}>
-          <img src={bottom} />
+        <div className='container-login'>
+          <a href='/lobby'>
+            <img className='position-absolute top-50 start-50 translate-middle'
+              src={bottom}
+              style={{ width: '45%', height: '100%', paddingTop: '20vw' }} />
+          </a>
+
+          <div className='centered-login'>
+            <h3 className='title-login'>Join a room</h3>
+
+          </div>
         </div>
+
+
       </div>
-    </div>
+
+
+
+
+
+    </div >
   );
 }
