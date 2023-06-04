482dface31b0ae294e60c4a24adb46b342c81652
Carolina Haggerty <carolina.vhaggerty@gmail.com>
2023-06-04 16:43:30 -0700

    changes

--- src/components/pages/InGame.js
+++ src/components/pages/InGame.js
@@ -4,11 +4,11 @@ import Board from './Board';
 import Lobby from './Lobby'
 import { io } from "socket.io-client";
 import { useParams } from "react-router-dom";
-// const local_url = 'http://localhost:4000/'
-const server_url = 'https://doodledash.herokuapp.com/'
+const local_url = 'http://localhost:4000/'
+// const server_url = 'https://doodledash.herokuapp.com/'
 
-// const socket = io(local_url);
-const socket = io(server_url);
+const socket = io(local_url);
+// const socket = io(server_url);
 
 
 function InGame({ username }) {
