e94d848fee2efb4f9da1509f8be8e186eab1b7dd
Alivia <43556891+17anguyen@users.noreply.github.com>
2023-06-04 14:29:48 -0700

    fixed helmet drop down

--- src/components/pages/RoomSelect.js
+++ src/components/pages/RoomSelect.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import '../css/UserSelect.css';
 import { Link } from 'react-router-dom'
-import { Helmet } from "react-helmet";
 import arrows from '../../Assets/page elements/arrows.svg'
 import planet from '../../Assets/page elements/planet.svg'
 
 export default function createUser() {
   return (
-    //     <Helmet>
-    //  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
-    //       </Helmet>
-
     <div className='UserSelectbg z-n1'>
       <img className='z-1 position-absolute top-0 start-0 mx-auto' src={planet} style={{ width: '20%' }} />
       <img className='z-1 position-absolute bottom-0 end-0 mx-auto' src={arrows} style={{ width: '10%' }} />
@@ -21,9 +16,9 @@ export default function createUser() {
         <div className='signup position-absolute top-50 start-50 translate-middle dropdown-center' >
           <h1 className="btn-text btn dropdown-toggle" style={{ color: '#37319D' }} type="button" data-bs-toggle="dropdown" aria-expanded="false">Select a room</h1>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">room 1</a></li>
-            <li><a class="dropdown-item" href="#">room 2</a></li>
-            <li><a class="dropdown-item" href="#">room 3</a></li>
+            <li><Link to="/game/1" className="dropdown-item" >Join Room 1</Link></li>
+            <li><Link to="/game/2" className="dropdown-item" >Join Room 2</Link></li>
+            <li><Link to="/game/3" className="dropdown-item" >Join Room 3</Link></li>
           </ul>
         </div>
         {/* </div> */}
