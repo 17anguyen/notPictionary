1bd26b8fbae6b0fa055802ea26550891b264a41b
Carolina Haggerty <carolina.vhaggerty@gmail.com>
2023-06-05 14:08:30 -0700

    fix url

--- src/components/pages/InGame.js
+++ src/components/pages/InGame.js
@@ -7,8 +7,8 @@ import { useParams } from "react-router-dom";
 // const local_url = 'http://localhost:4000'
 const server_url = 'https://doodledash.herokuapp.com/'
 
-const socket = io(local_url);
-// const socket = io(server_url);
+//const socket = io(local_url);
+const socket = io(server_url);
 
 
 function InGame({ username }) {
