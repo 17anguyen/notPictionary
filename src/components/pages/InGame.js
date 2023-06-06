c0b3ae70a9c980380bc88ad0851f5c87f2dc1c25
Anna <127552050+mercurybased@users.noreply.github.com>
2023-06-06 12:02:40 -0700

    Update InGame.js

--- src/components/pages/InGame.js
+++ src/components/pages/InGame.js
@@ -7,8 +7,8 @@ import { useParams } from "react-router-dom";
 import Word from "./Word"
 import "../css/InGame.css";
 
-const local_url = 'http://localhost:4000'
-// const server_url = 'https://doodledash.herokuapp.com/'
+// const local_url = 'http://localhost:4000'
+const server_url = 'https://doodledash.herokuapp.com/'
 
  const socket = io(local_url);
 // const socket = io(server_url);
