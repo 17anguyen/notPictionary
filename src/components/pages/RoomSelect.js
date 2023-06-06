a2658978089d9a83ef810d0b8f19c36c56e4cc45
DevManCryBB <veronicagriggs3@gmail.com>
2023-06-06 15:00:56 -0700

    merging dev

--- src/components/pages/RoomSelect.js
+++ src/components/pages/RoomSelect.js
@@ -4,17 +4,21 @@ import { Link } from 'react-router-dom'
 import API from '../../utils/api'
 import arrows from '../../Assets/page elements/arrows.svg'
 import planet from '../../Assets/page elements/planet.svg'
+import { Dropdown } from 'bootstrap';
 
 export default function RoomSelect() {
-
+  
   const freeRooms = async ()=> {    
+  
     try {
       const freeRoomsList = await API.getRooms();
+      const li= document.querySelectorAll('.test')
+      li.setAttribute("style", "")
       console.log(freeRoomsList)
       if (!freeRoomsList) {
         alert("No rooms available")
-      }else{
-        console.log(freeRoomsList)
+      }else {
+      console.log(freeRoomsList)
       }
     }catch(err){
       console.log(err)
@@ -26,6 +30,7 @@ export default function RoomSelect() {
    },[])
 
   return (
+  
     <div className='UserSelectbg z-n1'>
       <img className='z-1 position-absolute top-0 start-0 mx-auto' src={planet} style={{ width: '20%' }} />
       <img className='z-1 position-absolute bottom-0 end-0 mx-auto' src={arrows} style={{ width: '10%' }} />
@@ -36,9 +41,9 @@ export default function RoomSelect() {
         <div className='signup position-absolute top-50 start-50 translate-middle dropdown-center' >
           <h1 className="btn-text btn dropdown-toggle" style={{ color: '#37319D' }} type="button" data-bs-toggle="dropdown" aria-expanded="false">Select a room</h1>
           <ul className="dropdown-menu">
-            <li><Link to="/game/1" className="dropdown-item" >Join Room 1</Link></li>
-            <li><Link to="/game/2" className="dropdown-item" >Join Room 2</Link></li>
-            <li><Link to="/game/3" className="dropdown-item" >Join Room 3</Link></li>
+            <li style= {{display:'none'}} className='test' id='room1'><Link to="/game/1" className="dropdown-item" >Join Room 1</Link></li>
+            <li style= {{display:'none'}} className='test' id='room2'><Link to="/game/2" className="dropdown-item" >Join Room 2</Link></li>
+            <li style= {{display:'none'}} className='test' id='room3'><Link to="/game/3" className="dropdown-item" >Join Room 3</Link></li>
           </ul>
         </div>
         {/* </div> */}
