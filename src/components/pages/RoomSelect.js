2861ffe6aaa690c4eccfafc43872a7bc1c31c92b
DevManCryBB <veronicagriggs3@gmail.com>
2023-06-06 19:32:49 -0700

    updates

--- src/components/pages/RoomSelect.js
+++ src/components/pages/RoomSelect.js
@@ -4,15 +4,16 @@ import { Link } from 'react-router-dom'
 import API from '../../utils/api'
 import arrows from '../../Assets/page elements/arrows.svg'
 import planet from '../../Assets/page elements/planet.svg'
-import { Dropdown } from 'bootstrap';
+// import { Dropdown } from 'bootstrap';
 
 export default function RoomSelect() {
 
   const freeRooms = async () => {
     try {
+      
       const freeRoomsList = await API.getRooms();
-      const li = document.querySelector('.test')
-      li.setAttribute("style", "")
+      const li = document.querySelectorAll('.test')
+      li[0].setAttribute("style", "")
       console.log(freeRoomsList)
 
       if (!freeRoomsList) {
@@ -41,9 +42,9 @@ export default function RoomSelect() {
         <div className='signup position-absolute top-50 start-50 translate-middle dropdown-center' >
           <h1 className="btn-text btn dropdown-toggle" style={{ color: '#37319D' }} type="button" data-bs-toggle="dropdown" aria-expanded="false">Select a room</h1>
           <ul className="dropdown-menu">
-            <li style={{ display: 'none' }} className='test'><Link to="/game/1" className="dropdown-item" >Join Room 1</Link></li>
-            <li style={{ display: 'none' }} className='test'><Link to="/game/2" className="dropdown-item" >Join Room 2</Link></li>
-            <li style={{ display: 'none' }} className='test'><Link to="/game/3" className="dropdown-item" >Join Room 3</Link></li>
+            <li className='test dropdown-item'><Link to="/game/1" >Join Room 1</Link></li>
+            <li className='test dropdown-item'><Link to="/game/2" >Join Room 2</Link></li>
+            <li className='test dropdown-item'><Link to="/game/3" >Join Room 3</Link></li>
           </ul>
         </div>
         {/* </div> */}
