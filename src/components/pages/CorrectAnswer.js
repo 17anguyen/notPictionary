b6f1b7c0d5da3f2bc9ba6e61359b8db684a128db
Alivia <43556891+17anguyen@users.noreply.github.com>
2023-06-08 00:43:44 -0700

    started workign correct answer screen

--- src/components/pages/CorrectAnswer.js
+++ src/components/pages/CorrectAnswer.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import '../css/CorrectAnswer.css';
+import ring from '../../Assets/page elements/glowingRing.svg'
+import top from '../../Assets/page elements/accentTop.svg'
+import bottom from '../../Assets/page elements/accentBottom.svg'
 
 export default function CorrectAnswer() {
-    return  <div className='winninganswer'>
+    return <div className='winninganswer z-n1'>
 
+        <div className='container cansweroutline'>
+            <img className='ring position-absolute top-50 start-50 translate-middle' src={ring} style={{ width: '60%' }} />
+
+            <div className='answerbox-correct z-2'>
+                <h1>test</h1>
+            </div>
+            <div className='namebox-correct z-2'>
+                <h3>user name</h3>
+            </div>
+        </div>
+        <img className='' src={top} style={{ width: '15%' }} />
+
+        <img className='' src={bottom} style={{ width: '15%' }} />
     </div>
 }
